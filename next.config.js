80855e71f961e6866ac0f63904697d691395c74a
abu <2261983469@qq.com>
2026-03-06 19:29:18 +0800

    Fix basePath in next.config.js from /agents/lit-matrix to /agents/literature-matrix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   skipTrailingSlashRedirect: true,
-  basePath: '/agents/lit-matrix',
+  basePath: '/agents/literature-matrix',
   experimental: {
     instrumentationHook: true,
     serverActions: {
